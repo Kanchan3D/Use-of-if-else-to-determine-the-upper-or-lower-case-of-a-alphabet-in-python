b9c0271c1c213588e7c31d94af3ef998d327b4be
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-03-17 18:58:48 +0530

    Add files via upload

---
+++ alphabet.py
@@ -0,0 +1,8 @@
+print("enter a character:")
+c=input()
+if(c>='a'and c<='z'):
+    print( "It is a small case alphabet.")
+elif(c>='A'and c<='Z'):
+    print( "It is a capital case alphabet")
+else:
+    print("It is not a alphabet.")
